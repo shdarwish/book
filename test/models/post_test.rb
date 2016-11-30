d26ad60153f5f3905c28ff6c83dc3a56b1a9ba97
Shaikha Al-Darwish <sdarwish@qatar.cmu.edu>
2016-11-30 21:39:43 +0300

    firts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,11 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+   test "post attribute must not be empty" do 
+  	post = Post.new
+  	assert post.invalid?
+  	assert post.errors[:book_id].any?
+  	assert post.errors[:contant].any?
+  end
 end
--- test/models/quiz_test.rb
+++ test/models/quiz_test.rb
@@ -4,4 +4,17 @@ class QuizTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "quiz attribute must not be empty" do 
+  	quiz = Quiz.new
+  	assert quiz.invalid?
+  	assert quiz.errors[:q1].any?
+  	assert quiz.errors[:q2].any?
+  	assert quiz.errors[:q3].any?
+  	assert quiz.errors[:q4].any?
+  	assert quiz.errors[:q5].any?
+  	assert quiz.errors[:q6].any?
+  	assert quiz.errors[:q7].any?
+  	assert quiz.errors[:results].any?
+  end
 end
